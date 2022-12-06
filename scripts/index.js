a0fea747dbe9d74ea886520ca56c0b9548f55904
andreww28 <johnandrewsanvictores@gmail.com>
2022-12-06 23:31:38 +0800

    add functionalities on search page

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,16 @@
+const data = JSON.parse(item_data);
 const main = () => {
   document.querySelector(".option #desc").click();
   document.querySelector('.desc-content').style.display = 'flex';
   document.querySelector('.img-content').style.display = 'none';
+
+
+  insert_project_search();
 }
 
 
 const radio_show_content = (e) => {
   if(e.value == "image") {
-    console.log(document.querySelector('.desc-content'));
     document.querySelector('.desc-content').style.display = 'none';
     document.querySelector('.img-content').style.display = 'flex';
   }else if(e.value == "desc") {
@@ -15,5 +18,44 @@ const radio_show_content = (e) => {
     document.querySelector('.img-content').style.display = 'none';
   }
 }
+
+
+const insert_project_search = () => {
+  const mug1_img = data.mug[0]["img-link"];
+  const tshirt1_img = data.shirt[0]["img-link"];
+  const shoes1_img = data.shoes[0]["img-link"];
+  const slipper1_img = data.slippers[0]["img-link"];
+
+  append_projecs_search_child(mug1_img, "mug");
+  append_projecs_search_child(tshirt1_img, "shirt");
+  append_projecs_search_child(shoes1_img, "shoes");
+  append_projecs_search_child(slipper1_img, "slippers");
+
+  add_link_img();
+}
+
+
+const append_projecs_search_child = (img, obj_type) => {
+  const project_contents = document.querySelector('.project-contents');
+  var documentFragment = document.createRange().createContextualFragment(`
+  <div class="project-item">
+    <img src="` + img + `" id="` + obj_type + `">
+  </div>`);
+
+  project_contents.prepend(documentFragment);
+}
+
+const add_link_img = () => {
+  const project_content_items = document.querySelectorAll('.project-item');
+  var url = null;
+  project_content_items.forEach((item, i) => {
+      item.addEventListener("click", (e) => {
+        url = '../pages/search-result.html?name=' + encodeURIComponent(e.target.id);
+        document.location.href = url;
+      });
+  });
+
+}
+
 
 main();
---
+++ scripts/relevant-result.js
@@ -0,0 +1,39 @@
+const item_d = JSON.parse(item_data);
+
+window.onload = function () {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = tmp[1];
+    }
+
+    var item_chosen = data.name;
+    var items = item_d[item_chosen];
+    update_result_imgs(items);
+
+
+}
+
+
+const update_result_imgs = (items) => {
+  document.querySelector("#chosen-item-img").src = items[0]["img-link"];
+
+  items.forEach((item, i) => {
+    append_result_items_child(item["img-link"], item["product-name"], item["product-link"]);
+  });
+}
+
+
+const append_result_items_child = (img, name, link) => {
+  const result_items = document.querySelector('.result-items');
+  var documentFragment = document.createRange().createContextualFragment(`
+    <div class="result-item">
+      <img src="` + img + `">
+      <p class="result-item-name">` + name + `</p>
+      <a href="` + link + `" target="_blank">Item link</a>
+    </div>`);
+
+  result_items.appendChild(documentFragment);
+}
